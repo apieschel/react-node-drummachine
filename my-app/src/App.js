789de2857c295d2f185313828ce73e55f41e51e7
Alex <apieschel@gmail.com>
2019-08-22 16:55:18 +0000

    ✈️⛪️ Checkpoint ./my-app/src/App.js:2265073/72

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -187,14 +187,14 @@ class App extends Component {
       clearInterval(self.state.intervalId);
       for(let i = 0; i < files.length; i++) {
         
-        let audioSrc = "/public/music/" + directory + "/" + files[i];
+        const audioSrc = process.env.PUBLIC_URL + "/music/" + directory + "/" + files[i];
         
         api.loadAudio({directory: directory, file: files[i]}, function(data) {
           console.log(data);
         });
         
         
-        let track = {};
+        const track = {};
         track.steps = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
         track.playSound = new Audio(audioSrc);
         track.name = files[i];
